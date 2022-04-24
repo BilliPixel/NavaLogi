8085a21a5ac5c0ab9b1006f6bd2f8eb4a9c1eedb
David Oluwatosin Solomon <babasala35@gmail.com>
2022-04-24 12:53:01 +0100

    first commit

---
+++ js/custom.js
@@ -0,0 +1,135 @@
+(function ($) {
+
+  "use strict";
+
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').fadeOut(1000); // set duration in brackets    
+    });
+
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+          } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+          }
+    });
+
+
+    // HOME SLIDER & COURSES & CLIENTS
+    $('.home-slider').owlCarousel({
+      animateOut: 'fadeOut',
+      items:1,
+      loop:true,
+      dots:false,
+      autoplayHoverPause: false,
+      autoplay: true,
+      smartSpeed: 1000,
+    })
+
+    $('.owl-courses').owlCarousel({
+      animateOut: 'fadeOut',
+      loop: true,
+      autoplayHoverPause: false,
+      autoplay: false,
+      dots: false,
+      nav:true,
+      navText: [
+          '<i class="fa fa-angle-left"></i>',
+          '<i class="fa fa-angle-right"></i>'
+      ],
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        1000: {
+          items: 3,
+        }
+      }
+    });
+
+    $('.owl-client').owlCarousel({
+      animateOut: 'fadeOut',
+      loop: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      smartSpeed: 1000,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        1000: {
+          items: 3,
+        }
+      }
+    });
+
+
+    // SMOOTHSCROLL
+    $(function() {
+      $('.custom-navbar a, #home a').on('click', function(event) {
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+            event.preventDefault();
+      });
+    });  
+
+})(jQuery);
+
+
+window.addEventListener("DOMContentLoaded", function () {
+  // get the form elements defined in your form HTML above
+
+  var form = document.getElementById("my-form");
+  // var button = document.getElementById("my-form-button");
+  var status = document.getElementById("status");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Thanks, we will get back to you soon";
+  }
+
+  function error() {
+    status.classList.add("error");
+    status.innerHTML = "Oops! There was a problem.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
+
